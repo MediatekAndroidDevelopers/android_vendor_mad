109f5ee9eafdafbae46c2d0529e770e9fd6c07af
fire855 <thefire855@gmail.com>
2018-01-01 23:19:51 +0100

    config: set default wifi country code
    allows 5ghz wifi hotspot without a sim card

--- config/wifi.mk
+++ config/wifi.mk
@@ -12,3 +12,6 @@ PRODUCT_COPY_FILES += \
     vendor/mad/prebuilt/etc/wifi/wpa_supplicant.conf:system/vendor/etc/wifi/wpa_supplicant.conf \
     vendor/mad/prebuilt/etc/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
     vendor/mad/prebuilt/etc/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.wificountrycode=00
